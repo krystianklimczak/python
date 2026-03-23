c9477a3273119d2ec786addc212c1fb895e4fa9b
Krystian Klimczak <klimczak777@icloud.com>
2026-03-23 18:31:05 +0100

    causing error...

---
+++ 2. zmienne i proste typy danych/wrong_message.py
@@ -0,0 +1,8 @@
+message = 'Witaj, Czytelniku książki "Pythona. Instrukcje dla programisty"!'
+# print(mesage)
+
+# Traceback (most recent call last):
+# File "hello_world.py", line 2, in <module> 
+# print(mesage) 
+# ^^^^^^
+# NameError: name 'mesage' is not defined. Did you mean: 'message'? 
