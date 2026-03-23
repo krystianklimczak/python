f5ec70650a2b88e7f7f5cc32d1fed715884621bb
Krystian Klimczak <klimczak777@icloud.com>
2026-03-23 18:35:13 +0100

    printing string with methods: lower(), upper(), title()

---
+++ exercises/2/wielkosc_liter_w_imionach.py
@@ -0,0 +1,10 @@
+name = "Anna"
+
+# Zmiana imienia na małe litery
+print(name.lower())
+
+# Zmiana imienia na wielkie litery
+print(name.upper())
+
+# Zmiana imienia na tytułowe (pierwsza litera wielka, reszta małe)
+print(name.title())
