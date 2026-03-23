adf002061a81b91a878a91c7fe22ed165a2d7322
Krystian Klimczak <klimczak777@icloud.com>
2026-03-23 18:29:10 +0100

    apostrofy

---
+++ 2. zmienne i proste typy danych/apostrophe.py
@@ -0,0 +1,12 @@
+message = "Dla programisty Johna O'Hary jedną z zalet Pythona jest jego wszechstronność i oddana mu społeczność."
+
+# print(message)
+# Dla programisty Johna O'Hary jedną z zalet Pythona jest jego wszechstronność i oddana mu społeczność.
+
+# message = 'Dla programisty Johna O'Hary jedną z zalet Pythona jest jego wszechstronność i oddana mu społeczność.'
+
+# print(message)
+# File "/Users/krycha/Documents/python_projects/python/2. zmienne i proste typy danych/apostrophe.py", line 6
+#     message = 'Dla programisty Johna O'Hary jedną z zalet Pythona jest jego wszechstronność i oddana mu społeczność.'
+#                                                                                                                     ^
+# SyntaxError: unterminated string literal (detected at line 6)
