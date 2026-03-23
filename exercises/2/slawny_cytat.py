04546489ece2072a2bc6d2af8c99eac0ddd86d2a
Krystian Klimczak <klimczak777@icloud.com>
2026-03-23 18:33:34 +0100

    creating variable/string with other variables/strings

---
+++ exercises/2/slawny_cytat.py
@@ -0,0 +1,12 @@
+
+# Sławny cytat
+quote = "Jeśli debugowanie jest procesem usuwania błędów z oprogramowania, to programowanie musi być procesem ich umieszczania."
+
+# Autor cytatu
+quote_author = "Edsger W. Dijkstra"
+
+# Tworzenie komunikatu z cytatem
+text = f'{quote_author} powiedział kiedyś: "{quote}"'
+
+# Wyświetlanie komunikatu
+print(text)
