50efaf83e654a3fa384bd0799e69d5278ad1ac09
Krystian Klimczak <klimczak777@icloud.com>
2026-03-24 18:59:23 +0100

    creating list, creating message with one of the list item, printing message.

---
+++ 3. wprowadzenie do list/bicycles.py
@@ -0,0 +1,17 @@
+bicycles = ['trekingowy', 'górski', 'miejski', 'szosowy']
+
+# print(bicycles) // ['trekingowy', 'górski', 'miejski', 'szosowy']
+
+# print(bicycles[0])  # trekingowy
+
+# print(bicycles[0].title())  # Trekingowy
+
+# print(bicycles[1])  # górski
+# print(bicycles[3])  # szosowy
+
+# print(bicycles[-1])  # szosowy
+
+message = f"Moim pierwszym rowerem był rower {bicycles[0].title()}."
+
+print(message) # Moim pierwszym rowerem był rower Trekingowy.
+
