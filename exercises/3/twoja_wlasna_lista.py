2a343792dcf497e558dea26198d1eb9db4ce105d
Krystian Klimczak <klimczak777@icloud.com>
2026-03-24 19:05:01 +0100

    creating vehicles list and printing messages.

---
+++ exercises/3/twoja_wlasna_lista.py
@@ -0,0 +1,9 @@
+vehicles = ["samochód", "motocykl", "rower", "rolki"]
+
+print(f'Najczęściej do przemieszczania się przydaje mi się {vehicles[0]}.') # Najczęściej do przemieszczania się przydaje mi się samochód.
+
+print(f'Zawsze marzyłem, aby mieć szybki {vehicles[1]}.') # Zawsze marzyłem, aby mieć szybki motocykl.
+
+print(f'Jako mój głowny środek transportu do pracy używam {vehicles[2]}.') # Jako mój głowny środek transportu do pracy używam rower.
+
+print(f'Jeśli chcę się zrelaksować i wybrać na przejażdżkę, to wybieram {vehicles[3]}.') # Jeśli chcę się zrelaksować i wybrać na przejażdżkę, to wybieram rolki.
