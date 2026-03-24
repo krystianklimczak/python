b15a4461355a1f0a8e03748d62d7f887673235eb
Krystian Klimczak <klimczak777@icloud.com>
2026-03-24 19:03:24 +0100

    creating list with names, and printing welcome messages.

---
+++ exercises/3/powitania.py
@@ -0,0 +1,7 @@
+names = ["Ania", "Patryk", "Dawid", "Kasia", "Paula"]
+
+print(f"Witaj, {names[0]}! \tMiłego dnia!") # Witaj, Ania! Miłego dnia!
+print(f"Witaj, {names[1]}! \tMiłego dnia!") # Witaj, Patryk! Miłego dnia!
+print(f"Witaj, {names[2]}! \tMiłego dnia!") # Witaj, Dawid! Miłego dnia!
+print(f"Witaj, {names[3]}! \tMiłego dnia!") # Witaj, Kasia! Miłego dnia!
+print(f"Witaj, {names[4]}! \tMiłego dnia!") # Witaj, Paula! Miłego dnia!
