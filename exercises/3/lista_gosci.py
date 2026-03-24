40a840d9dbbf26bd07d1ee24cbe96acbe3f307ad
Krystian Klimczak <klimczak777@icloud.com>
2026-03-24 19:05:31 +0100

    creating quests list and printing invite messages.

---
+++ exercises/3/lista_gosci.py
@@ -0,0 +1,6 @@
+guests = ['Urszula', 'Tadeusz', 'Paula']
+
+print(f'Zapraszam na obiad, {guests[0]}.') # Zapraszam na obiad, Urszula.
+print(f'Zapraszam na obiad, {guests[1]}.') # Zapraszam na obiad, Tadeusz.
+print(f'Zapraszam na obiad, {guests[2]}.') # Zapraszam na obiad, Paula.
+
