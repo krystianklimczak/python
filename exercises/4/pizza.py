0bc076e5c8c901970ec08e8bec5263b8b0f75716
Krystian Klimczak <klimczak777@icloud.com>
2026-03-25 18:14:17 +0100

    creating pizzas list and printing them with some text.

---
+++ exercises/4/pizza.py
@@ -0,0 +1,14 @@
+pizzas = ['margarita', 'pepperoni', 'hawaiian', 'calzone', 'veggie']
+
+for pizza in pizzas:
+    print(pizza)
+
+# 
+
+for pizza in pizzas:
+    print(f'Lubię pizzę {pizza.title()}.')
+
+# 
+
+print('Uwielbiam pizzę! ')
+
