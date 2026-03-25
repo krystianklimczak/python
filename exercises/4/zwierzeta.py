163185c85b49979d6e9cb5849fb07afadc4860f5
Krystian Klimczak <klimczak777@icloud.com>
2026-03-25 18:15:58 +0100

    creating animals list, printing them, printing them with some text.

---
+++ exercises/4/zwierzeta.py
@@ -0,0 +1,13 @@
+animals = ['kot', 'pies', 'królik', 'chomik']
+
+for animal in animals:
+    print(animal)
+
+# 
+
+for animal in animals:
+    print(f'{animal.title()} to wspaniałe zwierzę!')
+
+# 
+
+print('Wszystkie te zwierzęta są świetnymi towarzyszami!')
