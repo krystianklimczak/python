67da76f03bd10ca4beca0fd9b80f41d367eff715
Krystian Klimczak <klimczak777@icloud.com>
2026-03-25 18:07:23 +0100

    genereting index error. IndexError: list index out of range.

---
+++ exercises/3/celowy_blad.py
@@ -0,0 +1,8 @@
+list = []
+
+print(list[-1]) # IndexError: list index out of range
+# Traceback (most recent call last):
+#   File "/Users/krycha/Documents/python_projects/python/exercises/3/celowy_blad.py", line 3, in <module>
+#     print(list[-1]) # IndexError: list index out of range
+#           ~~~~^^^^
+# IndexError: list index out of range
