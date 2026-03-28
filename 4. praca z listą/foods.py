83b5e973f4335771241c275010dd86b803fb6824
Krystian Klimczak <klimczak777@icloud.com>
2026-03-28 16:58:41 +0100

    copying list using slice [:].

---
+++ 4. praca z listą/foods.py
@@ -0,0 +1,22 @@
+my_foods = ['pizza', 'falafel', 'ciasto z marchwi']
+friend_foods = my_foods[:]
+
+print("Moje ulubione potrawy to:")
+print(my_foods)
+
+print("\nUlubione potrawy mojego przyjaciela to:")
+print(friend_foods)
+
+# 
+
+my_foods.append('cannolo')
+friend_foods.append('lody')
+
+print("\nMoje ulubione potrawy to:")
+print(my_foods)
+
+print("\nUlubione potrawy mojego przyjaciela to:")
+print(friend_foods)
+
+# 
+
