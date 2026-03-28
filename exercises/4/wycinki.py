94ba59efba96c077847327a999cc7bc91d1e8fa6
Krystian Klimczak <klimczak777@icloud.com>
2026-03-28 17:01:44 +0100

    making slices of a list and printing them.

---
+++ exercises/4/wycinki.py
@@ -0,0 +1,17 @@
+colors = ['red', 'green', 'blue', 'yellow', 'purple']
+
+# 
+
+print("Pierwsze trzy elementy listy to:")
+print(colors[0:3])
+
+# 
+
+print("\nTrzy elementy w środku listy to:")
+print(colors[1:4])
+
+# 
+
+print("\nOstatnie trzy elementy listy to:")
+print(colors[-3:])
+
