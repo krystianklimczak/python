3df8ac1f572a50c2e504958e4089b8609046ea1e
Krystian Klimczak <klimczak777@icloud.com>
2026-03-28 17:01:20 +0100

    making copy of a list and changing each one.

---
+++ exercises/4/wiecej_petli.py
@@ -0,0 +1,17 @@
+my_foods = ['pizza', 'falafel', 'ciasto z marchwi']
+friend_foods = my_foods[:]
+
+# 
+
+my_foods.append('cannolo')
+friend_foods.append('lody')
+
+# 
+
+print("Moje ulubione potrawy to:")
+for food in my_foods:
+    print(food)
+
+print("\nUlubione potrawy mojego przyjaciela to:")
+for food in friend_foods:
+    print(food)
