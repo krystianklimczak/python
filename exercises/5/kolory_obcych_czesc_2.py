48f5905e76387e91ffc22900af19f5be71f3b238
Krystian Klimczak <klimczak777@icloud.com>
2026-03-29 19:20:22 +0200

    construction if else.

---
+++ exercises/5/kolory_obcych_czesc_2.py
@@ -0,0 +1,15 @@
+alien_color = 'zielony'
+
+if alien_color == 'zielony':
+    print('Gracz zdobył 5 punktów!')
+else:
+    print('Gracz zdobył 10 punktów!')
+
+# 
+
+alien_color = 'żółty'
+
+if alien_color == 'zielony':
+    print('Gracz zdobył 5 punktów!')
+else:
+    print('Gracz zdobył 10 punktów!')
