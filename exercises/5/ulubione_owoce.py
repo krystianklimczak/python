10ad275347ccf696428b43066133054b3bde83b3
Krystian Klimczak <klimczak777@icloud.com>
2026-03-29 19:19:33 +0200

    construction with many if's, checking if item is in the list.

---
+++ exercises/5/ulubione_owoce.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['banan', 'jabłko', 'gruszka', 'truskawka', 'pomarańcza']
+
+if 'banan' in favorite_fruits:
+    print("Naprawdę lubisz banany!")
+if 'jabłko' in favorite_fruits:
+    print("Naprawdę lubisz jabłka!")
+if 'gruszka' in favorite_fruits:
+    print("Naprawdę lubisz gruszki!")
+if 'truskawka' in favorite_fruits:
+    print("Naprawdę lubisz truskawki!")
+if 'pomarańcza' in favorite_fruits:
+    print("Naprawdę lubisz pomarańcze!")
