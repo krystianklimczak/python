b43eb7bff25222f2caf6906ce620df5a03ce7277
Krystian Klimczak <klimczak777@icloud.com>
2026-03-29 19:16:56 +0200

    construction if. separately check if item is in the list.

---
+++ 5. konstrukcja if/toppings.py
@@ -0,0 +1,10 @@
+requested_toppings = ['pieczarki', 'podwójny ser']
+
+if 'pieczarki' in requested_toppings:
+    print("Dodaję pieczarki.")
+if 'peperoni' in requested_toppings:
+    print("Dodaję pepperoni.")
+if 'podwójny ser' in requested_toppings:
+    print("Dodaję podwójny ser.")
+
+print("\nTwoja pizza jest już gotowa.")
