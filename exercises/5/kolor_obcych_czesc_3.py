073299f9392d8756a47a14cf0cbe1aed84a6b9a1
Krystian Klimczak <klimczak777@icloud.com>
2026-03-29 19:21:05 +0200

    constructions if elif to check exact if color is same.

---
+++ exercises/5/kolor_obcych_czesc_3.py
@@ -0,0 +1,30 @@
+alien_color = 'zielony'
+
+if alien_color == 'zielony':
+    print('Gracz zdobył 5 punktów!')
+elif alien_color == 'żółty':
+    print('Gracz zdobył 10 punktów!')
+elif alien_color == 'czerwony':
+    print('Gracz zdobył 15 punktów!')
+
+# 
+
+alien_color = 'żółty'
+
+if alien_color == 'zielony':
+    print('Gracz zdobył 5 punktów!')
+elif alien_color == 'żółty':
+    print('Gracz zdobył 10 punktów!')
+elif alien_color == 'czerwony':
+    print('Gracz zdobył 15 punktów!')
+
+# 
+
+alien_color = 'czerwony'
+
+if alien_color == 'zielony':
+    print('Gracz zdobył 5 punktów!')
+elif alien_color == 'żółty':
+    print('Gracz zdobył 10 punktów!')
+elif alien_color == 'czerwony':
+    print('Gracz zdobył 15 punktów!')
