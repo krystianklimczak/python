cce801c37d2c54b795f0119f672f0156c820f89f
Krystian Klimczak <klimczak777@icloud.com>
2026-03-29 19:19:11 +0200

    conditional tests with '==' .

---
+++ exercises/5/testy_warunkowe.py
@@ -0,0 +1,15 @@
+color = 'red'
+
+print("Czy kolor == 'czerwony'? Przewiduje wartość True.")
+print(color == 'red')
+
+print("\nCzy kolor == 'zielony'? Przewiduje wartość False.")
+print(color == 'green')
+
+car = 'subaru'
+
+print("\nCzy samochód == 'subaru'? Przewiduje wartość True.")
+print(car == 'subaru')
+
+print("\nCzy samochód == 'audi'? Przewiduje wartość False.")
+print(car == 'audi')
