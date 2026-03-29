f262d7b2aad3c41e87162162d9fc4d5221c48ece
Krystian Klimczak <klimczak777@icloud.com>
2026-03-29 19:17:09 +0200

    construction if else.

---
+++ 5. konstrukcja if/voting_copy.py
@@ -0,0 +1,8 @@
+age = 17
+
+if age >= 18:
+    print("Możesz wziąć udział w głosowaniu!")
+    print("Czy zarejestrowałeś się już, aby móc głosować?")
+else:
+    print("Przykro nam, ale jesteś zbyt młody, aby głosować.")
+    print("Mozesz się zarejestrować po ukeńczeniu 18 lat.")
