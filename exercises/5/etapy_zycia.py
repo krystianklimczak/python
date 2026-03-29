246943a6586bc725bf3bc646d3acb2af277559d0
Krystian Klimczak <klimczak777@icloud.com>
2026-03-29 19:18:49 +0200

    construction if with some elif's.

---
+++ exercises/5/etapy_zycia.py
@@ -0,0 +1,14 @@
+age = 29
+
+if age < 2:
+    print("Jesteś niemowlakiem.")
+elif age < 4:
+    print("Jesteś dzieckiem, które uczy się chodzić.")
+elif age < 13:
+    print("Jesteś dzieckiem.")
+elif age < 20:
+    print("Jesteś nastolatkiem.")
+elif age < 65:
+    print("Jesteś dorosłym.")
+elif age >= 65:
+    print("Jesteś seniorem.")
