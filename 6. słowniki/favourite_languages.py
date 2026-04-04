01b5689fbf65ba30ac89486a105f520856812cfc
Krystian Klimczak <klimczak777@icloud.com>
2026-04-04 22:53:14 +0200

    creating dictionary with people and languages.

---
+++ 6. słowniki/favourite_languages.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+    "janek": "python",
+    "sara": "c",
+    "edward": "rust",
+    "paweł": "python",
+}
+
+language = favorite_languages["sara"].title()
+print(f"Ulubiony język programowania Sary to {language}.")
