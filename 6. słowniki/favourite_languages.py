d3b4798c7d5cc9de7cd28af890ee9e51a66387d6
Krystian Klimczak <klimczak777@icloud.com>
2026-04-05 15:34:53 +0200

    Iteration in dictionary with for key, value loop using method items(). Iteration through keys using keys() method. Checking if string is in dictionary keys using method keys() and 'not in'.  For loop on sorted keys using sorted() method. For loop through unique set of dictionary values using set() method.

--- 6. słowniki/favourite_languages.py
+++ 6. słowniki/favourite_languages.py
@@ -7,3 +7,63 @@ favorite_languages = {
 
 language = favorite_languages["sara"].title()
 print(f"Ulubiony język programowania Sary to {language}.")
+
+#
+
+for name, language in favorite_languages.items():
+    print(f"\n{name.title()}, Twój ulubiony język to: {language.title()}!")
+
+#
+
+for name in favorite_languages.keys():  # Same as: for name in favorite_langugages:
+    print(name.title())
+
+#
+
+friends = ["paweł", "sara"]
+
+for name in favorite_languages.keys():
+    print(f"Witaj, {name.title()}.")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(
+            f"\tWitaj, {name.title()}! Widzę, że Twoim ulubionym językiem programowania jest {language}!"
+        )
+
+#
+
+if "elżbieta" not in favorite_languages.keys():
+    print("\nElżbieta, proszę weź udział w naszej ankiecie!")
+
+#
+
+favorite_languages = {
+    "janek": "python",
+    "sara": "c",
+    "edward": "rust",
+    "paweł": "python",
+}
+
+for name in sorted(favorite_languages.keys()):
+    print(f"\n{name.title()}, dziękujemy za udział w ankiecie.")
+
+#
+
+favorite_languages = {
+    "janek": "python",
+    "sara": "c",
+    "edward": "rust",
+    "paweł": "python",
+}
+
+print("\nW ankiecie zostały wymienione następujące języki programowania:")
+
+for language in favorite_languages.values():
+    print(language.title())
+
+#
+
+print("\nW ankiecie zostały wymienione następujące języki programowania:")
+for language in set(favorite_languages.values()):
+    print(language.title())
