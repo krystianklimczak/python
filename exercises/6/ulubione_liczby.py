bd03a62e6651d008f0639cbce6fa3151158b94d0
Krystian Klimczak <klimczak777@icloud.com>
2026-04-06 21:17:51 +0200

    nesting list inside dictionary and looping throgh to display texts.

--- exercises/6/ulubione_liczby.py
+++ exercises/6/ulubione_liczby.py
@@ -11,3 +11,17 @@ print(f"Ulubiona liczba Ani to: {favourite_numbers['ania']}.")
 print(f"Ulubiona liczba Ariny to: {favourite_numbers['arina']}.")
 print(f"Ulubiona liczba Pauli to: {favourite_numbers['paula']}.")
 print(f"Ulubiona liczba Kazimierza to: {favourite_numbers['kazimierz']}.")
+
+#
+
+favorite_numbers = {
+    "krystian": [15, 96],
+    "ania": [12, 25],
+    "arina": [7, 5],
+    "paula": [29, 13],
+}
+
+for person, fav_nums in favorite_numbers.items():
+    print(f"\nUlubione liczby użytkownika {person.title()} to:")
+    for num in fav_nums:
+        print(f"\t{num}")
