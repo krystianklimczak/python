7439e679e226f0ab2c51e9a05ddca30717412aee
Krystian Klimczak <klimczak777@icloud.com>
2026-04-09 09:25:42 +0200

    creating two line input from user and printing it.

---
+++ 7. dane wejściowe użytkownika i pętla while/greeter.py
@@ -0,0 +1,8 @@
+# name = input("Podaj swoje imię: ")
+# print(f"Witaj, {name}!")
+
+prompt = "Jeżeli powiesz nam, kim jesteś, spersonalizujemy wyświetlany komunikat."
+prompt += "\nJak masz na imię? "
+
+name = input(prompt)
+print(f"\nWitaj, {name}!")
