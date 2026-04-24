683d81f4b26e2fdaa4ebdd0caeada49f651f1e24
Krystian Klimczak <klimczak777@icloud.com>
2026-04-24 13:48:17 +0200

    creating simple function with one parameter.

---
+++ exercises/8/ulubiona_ksiazka.py
@@ -0,0 +1,6 @@
+def favorite_book(title):
+    """Wyświetla komunikat o ulubionej książce."""
+    print(f"Jedną z moich ulubionych książek jest '{title}'.")
+
+
+favorite_book("Python, instrukcje dla programisty. Wydanie III")
