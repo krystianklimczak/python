e618a84efddae5234bb3ad4662839d870d240658
Krystian Klimczak <klimczak777@icloud.com>
2026-04-24 13:47:53 +0200

    creating simple function to display message.

---
+++ exercises/8/komunikat.py
@@ -0,0 +1,8 @@
+def display_messages():
+    """Wyświetla informacje na temat tego czego uczę się w tym rozdziale."""
+    print(
+        "W tym rozdziale poznaje definiowanie funkcji, wywołanie funkcji oraz prace z parametrami i argumentami funkcji."
+    )
+
+
+display_messages()
