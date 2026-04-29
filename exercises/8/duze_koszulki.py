0fd013f2c15d206d594e2e7d666ea41a0bb296ae
Krystian Klimczak <klimczak777@icloud.com>
2026-04-29 16:12:23 +0200

    simple function with two default parameters.

---
+++ exercises/8/duze_koszulki.py
@@ -0,0 +1,10 @@
+def make_shirt(text="Uwielbiam Pythona", size="XL"):
+    """Wyświetla informoacje dotyczące zamówionej koszulki."""
+    print(f"\nKoszulka którą zamówiłeś będzie rozmiaru: '{size}'.")
+    print(f"Tekst, który znajdzie się na koszulce to: '{text}'.")
+
+
+make_shirt()
+make_shirt(size="L")
+make_shirt(text="Python jest spoko!")
+make_shirt(text="Python giga czad", size="S")
