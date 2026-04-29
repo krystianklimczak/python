07b44d7b242f30c745e76147d8d198be925f6c10
Krystian Klimczak <klimczak777@icloud.com>
2026-04-29 16:11:46 +0200

    simple function with two parameters.

---
+++ exercises/8/t-shirt.py
@@ -0,0 +1,8 @@
+def make_shirt(size, text):
+    """Wyświetla informoacje dotyczące zamówionej koszulki."""
+    print(f"\nKoszulka którą zamówiłeś będzie rozmiaru: '{size}'.")
+    print(f"Tekst, który znajdzie się na koszulce to: '{text}'.")
+
+
+make_shirt("L", "Giga Czad")
+make_shirt(size="XL", text="Jak Pączek w Maśle!")
