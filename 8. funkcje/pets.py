fa497b2ed4c32850deff6d7376d12504b7c673eb
Krystian Klimczak <klimczak777@icloud.com>
2026-04-29 16:11:21 +0200

    functions with named arguments and default parameters.

--- 8. funkcje/pets.py
+++ 8. funkcje/pets.py
@@ -6,3 +6,21 @@ def describe_pet(animal_type, pet_name):
 
 describe_pet("chomik", "harry")
 describe_pet("pies", "willie")
+
+
+def describe_pet(animal_type, pet_name):
+    """Wyświetla informacje o zwierzęciu."""
+    print(f"\nMoje zwierzę to {animal_type}.")
+    print(f"Mój {animal_type} ma na imię {pet_name.title()}.")
+
+
+describe_pet(animal_type="chomik", pet_name="harry")
+
+
+def describe_pet(pet_name, animal_type="pies"):
+    """Wyświetla informacje o zwierzęciu."""
+    print(f"\nMoje zwierzę to {animal_type}.")
+    print(f"Mój {animal_type} ma na imię {pet_name.title()}.")
+
+
+describe_pet(pet_name="willie")
