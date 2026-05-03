ff34638b8141a3e0a1c03a744b8cb3030b69289b
Krystian Klimczak <klimczak777@icloud.com>
2026-05-03 12:27:17 +0200

    creating function to display city and country .

---
+++ exercises/8/nazwy_miast.py
@@ -0,0 +1,7 @@
+def city_country(city, country):
+    print(f"{city.title()}, {country.title()}")
+
+
+city_country("głogów", "polska")
+city_country("ascheberg", "niemcy")
+city_country("paryż", "francja")
